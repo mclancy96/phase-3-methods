b1a70e31e9674583ec51a724a07bfa9af7ca54e6
Mike <mclancy96@gmail.com>
2025-07-14 10:56:57 -0400

    Create methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,19 @@
-# Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(user)
+  puts "Hello, #{user}!"
+end
+
+def greet_with_default(user = "programmer")
+  puts "Hello, #{user}!"
+end
+
+def add(a,b)
+  a + b
+end
+
+def halve(int)
+  int.class.name == 'Integer' ? int / 2 : nil
+end
